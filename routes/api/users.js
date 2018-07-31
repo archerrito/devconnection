c39b0e705475d60dd8ad2cff060fd3f24ae50808
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-31 08:46:38 -0500

    add server routes

---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//Serve JSON
+// @ route GET api/users/test
+// @desc Test post route
+// @access Public
+router.get('/archer', (req, res) => res.json({msg: "Users Works"}));
+
+//export for server to pick up
+module.exports = router;
