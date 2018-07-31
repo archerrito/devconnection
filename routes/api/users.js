031ce4d18e646c3dd99185701b6ad7313b11d9ee
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-31 11:35:09 -0500

    add register and login email validators

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 //Load user model
 const User =   require('../../models/User');
 
@@ -20,12 +23,19 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 // @desc Register User
 // @access Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     //Find if email exists in Mongoose
     User.findOne({ email: req.body.email})
         .then(user => {
             //verifys user exists
             if(user) {
-                return res.status(400).json({email:'Email already exists'});
+                errors.email = 'Email already exists';
+                return res.status(400).json({errors});
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200', //size
@@ -62,6 +72,12 @@ router.post('/register', (req, res) => {
 // @desc Login User / Returning JWT token
 // @access Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -70,7 +86,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             //Check for user
             if(!user) {
-                return res.status(404).json({email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
             //compare password inserted to the database
             bcrypt.compare(password, user.password)
@@ -97,7 +114,8 @@ router.post('/login', (req, res) => {
                                 })
                         });
                     } else {
-                        return res.status(400).json({password: 'Password incorrect'});
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
                     }
                 })
         });
