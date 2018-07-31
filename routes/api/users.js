2bbdccab6aec39f7de495d12dbe049e9cd7527ab
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-07-31 09:39:58 -0500

    set login api

--- routes/api/users.js
+++ routes/api/users.js
@@ -55,5 +55,31 @@ router.post('/register', (req, res) => {
         })
 });
 
+// @route GET api/users/login
+// @desc Login User / Returning JWT token
+// @access Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({email})
+        .then(user => {
+            //Check for user
+            if(!user) {
+                return res.status(404).json({email: 'User not found'});
+            }
+            //compare password inserted to the database
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({msg: 'Success'});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect'});
+                    }
+                })
+        });
+});
+
 //export for server to pick up
 module.exports = router;
