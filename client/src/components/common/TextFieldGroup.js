4159b89556435cb1c1e698623a5de4eb07300bf4
Archer Hobson <supremeoverlord@Archers-MacBook-Pro.local>
2018-08-02 13:11:55 -0500

    add common input field groups for create-profile

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -15,20 +15,20 @@ const TextFieldGroup = ({
 }) => {
   return (
     <div className="form-group">
-    <input 
-      type={type}
-      className={classnames('form-control form-control-lg', {
-        'is-invalid' : error
-      })} 
-      placeholder={placeholder}
-      name={name} 
-      value={value}
-      onChange={onChange}
-      disabled={disabled}
-    />
-    {info && <small className="form-text" text-muted>{info}</small>}
-    {error && <div className="invalid-feedback">{error}</div>}
-  </div>
+      <input 
+        type={type}
+        className={classnames('form-control form-control-lg', {
+          'is-invalid' : error
+        })} 
+        placeholder={placeholder}
+        name={name} 
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {info && <small className="form-text" text-muted>{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
   );
 };
 
@@ -40,7 +40,7 @@ TextFieldGroup.propTypes = {
     error: PropTypes.string,
     type: PropTypes.string.isRequired,
     onChange: PropTypes.string.isRequired,
-    disabled: PropTypes.string,
+    disabled: PropTypes.string
 }
 
 TextFieldGroup.defaultProps = {
